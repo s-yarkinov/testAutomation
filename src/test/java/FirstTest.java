6a766173956604b33a7322fce008b1325d41fb5b
Yarkinov <s.yarkinov@ya.ru>
2020-12-07 11:43:04 +0500

    Home work test

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -8,29 +8,5 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import java.net.URL;
 
 public class FirstTest {
-    private AppiumDriver driver;
 
-    @Before
-    public void setUp() throws Exception {
-        DesiredCapabilities cap = new DesiredCapabilities();
-        cap.setCapability("platformName", "Android");
-        cap.setCapability("deviceName", "emulator-5554");
-        cap.setCapability("platformVersion", "7.1.1");
-        cap.setCapability("automationName", "Appium");
-        cap.setCapability("appPackage", "org.wikipedia");
-        cap.setCapability("appActivity", "main.MainActivity");
-        cap.setCapability("app", "/Users/macmini2/IdeaProjects/testAutomation/apps/org.wikipedia_50336.apk");
-
-        driver = new AndroidDriver(new URL("http://0.0.0.0:4723/wd/hub"), cap);
-    }
-
-    @After
-    public void tearDown(){
-        driver.quit();
-    }
-
-    @Test
-    public void firstTestRun(){
-        System.out.println("FirsttestRun");
-    }
 }
