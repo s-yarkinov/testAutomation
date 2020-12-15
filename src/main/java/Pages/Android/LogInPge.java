b9eee87403aba8e8194a0daee00871034b65d496
Yarkinov <s.yarkinov@ya.ru>
2020-12-15 11:24:38 +0500

    Assertions

---
+++ src/main/java/Pages/Android/LogInPge.java
@@ -0,0 +1,14 @@
+package Pages.Android;
+
+import com.codeborne.selenide.SelenideElement;
+import io.appium.java_client.MobileElement;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class LogInPge {
+    private SelenideElement nextButton = $(By.id("com.shazzle.nativechat:id/nextButton"));
+    private SelenideElement skipButton = $(By.id("com.shazzle.nativechat:id/skipButton"));
+
+
+}
--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,9 +1,14 @@
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
 
@@ -22,6 +27,12 @@ public class FirstTest {
         cap.setCapability("app", "/Users/macmini2/IdeaProjects/testAutomation/apps/org.wikipedia_50336.apk");
 
         driver = new AndroidDriver(new URL("http://0.0.0.0:4723/wd/hub"), cap);
+
+        waiteForElementAndClick(
+                By.id("org.wikipedia:id/fragment_onboarding_skip_button"),
+                "Skip button not found",
+                5
+        );
     }
 
     @After
@@ -30,8 +41,139 @@ public class FirstTest {
     }
 
     @Test
-    public void firstTestRun(){
-        System.out.println("FirsttestRun");
+    public void firstTestRun() throws InterruptedException {
+
+        waiteForElementAndClick(
+                By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
+                "Cannot find 'Search Wikipedia' element",
+                1
+        );
+        waiteForElementAndSendKeys(
+                By.xpath("//android.widget.LinearLayout/android.widget.EditText"),
+                "Java",
+                "Cannot SendKeys to element 'SearchField'",
+                1
+        );
+        waitForElementPresent(
+                By.xpath("//*[@resource-id = 'org.wikipedia:id/search_results_list']//*[contains(@text, 'Object-oriented programming language')]"),
+                "Result not found"
+        );
+    }
+
+    @Test
+    public void testCancelSearch() {
+        waiteForElementAndClick(
+                By.id("org.wikipedia:id/search_container"),
+                "Cannot find element",
+                1
+        );
+        waiteForElementAndClick(
+                By.xpath("//android.view.ViewGroup/android.widget.ImageButton"),
+                "Back button not found",
+                3
+        );
+
+        waiteForElementNotPresent(
+                By.xpath("//android.view.ViewGroup/android.widget.ImageButton"),
+                "X is still present on the screen",
+                5);
+    }
+
+    @Test
+    public void clearTextField() throws InterruptedException {
+        waiteForElementAndClick(
+                By.id("org.wikipedia:id/search_container"),
+                "Cannot find element",
+                1
+        );
+
+        waiteForElementAndSendKeys(
+                By.id("org.wikipedia:id/search_src_text"),
+                "Java",
+                "Text field not found",
+                3
+        );
+
+        waitForElementAndClear(
+                By.id("org.wikipedia:id/search_src_text"),
+                "Element 'search field' not found"
+        );
+    }
+
+    @Test
+    public void testCompareArticleTitle() {
+        waiteForElementAndClick(
+                By.id("org.wikipedia:id/search_container"),
+                "Cannot find element",
+                1
+        );
+
+        waiteForElementAndSendKeys(
+                By.id("org.wikipedia:id/search_src_text"),
+                "Java",
+                "Text field not found",
+                3
+        );
+
+        waiteForElementAndClick(
+                By.xpath("//*[@resource-id = 'org.wikipedia:id/search_results_list']//*[contains(@text, 'Object-oriented programming language')]"),
+                "Result not found",
+                3
+        );
+        WebElement titleElement = waitForElement(
+                By.xpath("//android.view.View/android.view.View[1]/android.view.View[1]"),
+                "Title desc not found",
+                15
+        );
+
+        String articleTitle = titleElement.getAttribute("text");
+
+        Assert.assertEquals(
+                "We see unexpected title",
+                "Java (programming language)",
+                articleTitle
+        );
+
+    }
+
+
+
+    private WebElement waitForElement(By by, String error_msg, long timeoutSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, timeoutSeconds);
+        wait.withMessage(error_msg + "\n");
+        return wait.until(
+                ExpectedConditions.presenceOfElementLocated(by)
+        );
+    }
+
+    private WebElement waitForElementPresent(By by, String error_msg) {
+        return  waitForElement(by, error_msg, 5);
+    }
+
+    private WebElement waiteForElementAndClick(By by, String err_msg, long timeoutSeconds){
+        WebElement element = waitForElementPresent(by, err_msg);
+        element.click();
+        return element;
+    }
+
+    private WebElement waiteForElementAndSendKeys(By by, String value, String err_msg, long timeoutSeconds){
+        WebElement element = waitForElementPresent(by, err_msg);
+        element.sendKeys(value);
+        return element;
+    }
+
+    private boolean waiteForElementNotPresent(By by, String err_msg, long timeoutSeconds){
+        WebDriverWait wait = new WebDriverWait(driver, timeoutSeconds);
+        wait.withMessage(err_msg + "\n");
+        return wait.until(
+                ExpectedConditions.invisibilityOfElementLocated(by)
+        );
+    }
+
+    private WebElement waitForElementAndClear(By by, String err_msg) throws InterruptedException {
+        WebElement element = waitForElementPresent(by, err_msg);
+        element.clear();
+        return element;
     }
 
 }
