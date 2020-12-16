05d9f1da532712119ef43af12312e036cdb6b829
Yarkinov <s.yarkinov@ya.ru>
2020-12-16 15:20:06 +0500

    Add swipeUp

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,17 +1,27 @@
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.touch.WaitOptions;
+import io.appium.java_client.touch.offset.PointOption;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
+import java.time.Duration;
 import java.util.List;
+
+import static io.appium.java_client.touch.WaitOptions.waitOptions;
+import static io.appium.java_client.touch.offset.PointOption.point;
+import static java.time.Duration.ofSeconds;
+
 
 public class FirstTest {
     private AppiumDriver driver;
@@ -251,6 +261,45 @@ public class FirstTest {
         }
     }
 
+    @Test
+    public void testSwipeArticle(){
+        waiteForElementAndClick(
+                By.id("org.wikipedia:id/search_container"),
+                "Cannot find element",
+                1
+        );
+
+        assertElementHasText(
+                By.id("org.wikipedia:id/search_src_text"),
+                "Search Wikipedia",
+                "Search Field not equals 'Search Wikipedia'"
+        );
+
+        waiteForElementAndSendKeys(
+                By.id("org.wikipedia:id/search_src_text"),
+                "Java",
+                "Text field not found",
+                15
+        );
+
+        waiteForElementAndClick(
+                By.xpath("//*[@resource-id = 'org.wikipedia:id/search_results_list']//*[contains(@text, 'Object-oriented programming language')]"),
+                "Result not found",
+                3
+        );
+        WebElement titleElement = waitForElement(
+                By.xpath("//android.view.View/android.view.View[1]/android.view.View[1]"),
+                "Title desc not found",
+                15
+        );
+
+        swipeUp(2);
+        swipeUp(2);
+        swipeUp(2);
+
+
+    }
+
 
 
     private WebElement waitForElement(By by, String error_msg, long timeoutSeconds) {
@@ -312,4 +361,17 @@ public class FirstTest {
         );
     }
 
+    private void swipeUp(int timeOfSwipe) {
+
+        TouchAction action = new TouchAction(driver);
+
+        Dimension size = driver.manage().window().getSize();
+
+        int x = size.width / 2;
+        int start_y = (int)(size.height * 0.8);
+        int end_y = (int)(size.height * 0.2);
+
+        action.press(point(x, start_y)).waitAction(waitOptions(ofSeconds(timeOfSwipe))).
+                moveTo(point(x, end_y)).release().perform();
+    }
 }
