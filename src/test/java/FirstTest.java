ed596f3a475d9e831b794fb565f1d0a4ac245c91
Yarkinov <s.yarkinov@ya.ru>
2020-12-18 19:16:46 +0500

    Home work: Ex5

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,6 +1,8 @@
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
 import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.touch.LongPressOptions;
 import io.appium.java_client.touch.WaitOptions;
 import org.junit.After;
 import org.junit.Assert;
@@ -11,6 +13,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.ScreenOrientation;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -303,7 +306,7 @@ public class FirstTest {
     @Test
     public void testSaveArticle() {
 
-        String listName = "test";
+        String readingListName = "test";
         waiteForElementAndClick(
                 By.id("org.wikipedia:id/search_container"),
                 "Cannot find element",
@@ -361,7 +364,7 @@ public class FirstTest {
 
         waiteForElementAndSendKeys(
                 By.id("org.wikipedia:id/text_input"),
-                listName,
+                readingListName,
                 "'Text input' element not found",
                 5
         );
@@ -385,7 +388,7 @@ public class FirstTest {
         );
 
         waiteForElementAndClick(
-                By.xpath("//*[@resource-id = 'org.wikipedia:id/item_title'][@text = '" + listName +"']"),
+                By.xpath("//*[@resource-id = 'org.wikipedia:id/item_title'][@text = '" + readingListName +"']"),
                 "List not found",
                 5
         );
@@ -513,7 +516,7 @@ public class FirstTest {
     }
 
     @Test
-    public void testChechSearchInBackground() {
+    public void testCheckSearchInBackground() {
         waiteForElementAndClick(
                 By.id("org.wikipedia:id/search_container"),
                 "Cannot find element",
@@ -548,6 +551,172 @@ public class FirstTest {
         );
     }
 
+    @Test
+    public void saveTwoArticles(){
+        String readingListName = "Test1";
+        String firstSubtitle = "//*[@resource-id = 'org.wikipedia:id/search_results_list']//*[contains(@text, 'Object-oriented programming language')]";
+        String secondSubtitle = "//*[@resource-id = 'org.wikipedia:id/search_results_list']//*[contains(@text, 'Programming language')]";
+
+        By titleOfArticle = By.id("org.wikipedia:id/view_page_title_text");
+
+        waiteForElementAndClick(
+                By.id("org.wikipedia:id/search_container"),
+                "Cannot find element",
+                1
+        );
+
+        assertElementHasText(
+                By.id("org.wikipedia:id/search_src_text"),
+                "Search…",
+                "Search Field not equals 'Search Wikipedia'"
+        );
+
+        waiteForElementAndSendKeys(
+                By.id("org.wikipedia:id/search_src_text"),
+                "Java",
+                "Text field not found",
+                15
+        );
+
+        waiteForElementAndClick(
+                By.xpath(firstSubtitle),
+                "Element not found: " + firstSubtitle,
+                3
+        );
+
+        String firstArticleTitle = waiteElementAndGetAttribute(
+                By.id("org.wikipedia:id/view_page_title_text"),
+                "text"
+        );
+
+
+        waiteForElementAndClick(
+                By.xpath("//android.widget.ImageView[@content-desc='More options']"),
+                "Element 'More option' not found",
+                5
+        );
+
+        waiteForElementAndClick(
+                By.xpath("//*[@text = 'Add to reading list']"),
+                "'Add to read list' element not found",
+                5
+        );
+
+        waiteForElementAndClick(
+                By.id("org.wikipedia:id/onboarding_button"),
+                "Button 'GOT IT' not found",
+                5
+        );
+
+        waitForElementAndClear(
+                By.id("org.wikipedia:id/text_input"),
+                "'Text input' element not found"
+        );
+
+        waiteForElementAndSendKeys(
+                By.id("org.wikipedia:id/text_input"),
+                readingListName,
+                "'Text input' element not found",
+                5
+        );
+
+        waiteForElementAndClick(
+                By.id("android:id/button1"),
+                "Not found 'OK' button",
+                5
+        );
+
+        waiteForElementAndClick(
+                By.xpath("//android.widget.ImageButton[@content-desc=\"Navigate up\"]"),
+                "Element 'X' not found",
+                5
+        );
+
+        waiteForElementAndClick(
+                By.id("org.wikipedia:id/search_container"),
+                "Cannot find element",
+                1
+        );
+
+        assertElementHasText(
+                By.id("org.wikipedia:id/search_src_text"),
+                "Search…",
+                "Search Field not equals 'Search Wikipedia'"
+        );
+
+        waiteForElementAndSendKeys(
+                By.id("org.wikipedia:id/search_src_text"),
+                "Java",
+                "Text field not found",
+                15
+        );
+
+        waiteForElementAndClick(
+                By.xpath(secondSubtitle),
+                "Element not found: " + secondSubtitle,
+                3
+        );
+
+        String secondArticleTitle = waiteElementAndGetAttribute(
+                By.id("org.wikipedia:id/view_page_title_text"),
+                "text"
+        );
+
+        waiteForElementAndClick(
+                By.xpath("//android.widget.ImageView[@content-desc='More options']"),
+                "Element 'More option' not found",
+                5
+        );
+
+        waiteForElementAndClick(
+                By.xpath("//*[@text = 'Add to reading list']"),
+                "'Add to read list' element not found",
+                5
+        );
+
+        waiteForElementAndClick(
+                By.xpath("//*[@text = '" + readingListName + "']"),
+                "Reading list not found",
+                5
+        );
+
+        waiteForElementAndClick(
+                By.xpath("//android.widget.ImageButton[@content-desc=\"Navigate up\"]"),
+                "Element 'X' not found",
+                5
+        );
+
+        waiteForElementAndClick(
+                By.xpath("//android.widget.FrameLayout[@content-desc=\"My lists\"]"),
+                "'My list' element not found",
+                5
+        );
+
+        waiteForElementAndClick(
+                By.xpath("//*[@resource-id = 'org.wikipedia:id/item_title'][@text = '" + readingListName +"']"),
+                "List not found",
+                5
+        );
+
+        swipeToLeftElement(
+                By.xpath("//*[@text = '" + firstArticleTitle +"']/../../.."),
+                "Article '" + firstArticleTitle + "' not found",
+                5
+        );
+
+        waiteForElementAndClick(
+                By.xpath("//*[@text = '" + secondArticleTitle +"']"),
+                "Article '" + secondArticleTitle + "' not found",
+                15
+        );
+
+        assertElementHasText(
+                titleOfArticle,
+                secondArticleTitle,
+                "The title of the article is different"
+        );
+    }
+
 
 
 
@@ -575,6 +744,14 @@ public class FirstTest {
         element.click();
         return element;
     }
+
+//    private WebElement waiteForElementAndLongPress(By by, String err_msg, long timeoutSeconds) {
+//        MobileElement element = (MobileElement) waitForElement(by, err_msg, timeoutSeconds);
+//
+//        TouchAction action = new TouchAction(driver);
+//
+//        action.longPress(LongPressOptions.longPressOptions());
+//    }
 
     private WebElement waiteForElementAndSendKeys(By by, String value, String err_msg, long timeoutSeconds){
         WebElement element = waitForElementPresent(by, err_msg);
